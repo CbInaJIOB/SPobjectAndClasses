a2aaaa3825cfae8ae03dc3c57b2af1e5047c7c8f
EngPG <sypalov_sergey@mail.ru>
2025-04-23 11:35:14 +0300

    add homework

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Author tolstoy = new Author("Лев", "Толстой");
+        Author lermontov = new Author("Михаил", "Лермонтов");
+
+        Book warAndPieace = new Book("Война и мир", tolstoy, 1869);
+        Book herro = new Book("Герой нашего времени", lermontov, 1841);
+
+        System.out.println(warAndPieace.getTitle() + " " + warAndPieace.getPublicationYear());
+        System.out.println(herro.getTitle() + " " + herro.getPublicationYear());
+
+        warAndPieace.setPublicationYear(2022);
+
+        System.out.println(warAndPieace.getTitle() + " " + warAndPieace.getPublicationYear());
     }
 }
